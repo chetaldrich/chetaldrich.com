5b54909edc854b41f7c8ac89b1b4b6eed36e5f3b
chetaldrich <chetaldrich@gmail.com>
2014-12-07 16:34:18 -0800

    Implemented article deletion

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -24,6 +24,12 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy 
+        redirect_to articles_path
+    end
 
     def update
         @article = Article.find(params[:id])
