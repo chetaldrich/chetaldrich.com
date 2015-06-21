569119322f4cdf1ed2bb695a2d3d78b9d80f3976
chetaldrich <chetaldrich@gmail.com>
2015-06-21 11:24:33 -0700

    Add scroll animation to button

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -2,4 +2,10 @@
 //= require "modernizr"
 //= require "bootstrap"
 //= require "skillbar"
-//= require "animatescroll/animatescroll.js"
+
+// creates scroll on click from down arrow on the home page
+$(document).ready(function() {
+    $(".down_arrow").click(function() {
+        $("body").animate({ scrollTop: $('#about').offset().top }, 1000);
+    });
+});
