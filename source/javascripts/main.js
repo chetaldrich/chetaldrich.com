5991d2c92491f50c82c90a7c7280446e8b3ca090
chetaldrich <chetaldrich@gmail.com>
2015-06-21 17:56:48 -0700

    Add cycling background on home, new images

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -5,6 +5,13 @@
 
 // creates scroll on click from down arrow on the home page
 $(document).ready(function() {
+
+    var imgs = ["banner1.jpg","banner2.jpg","banner3.jpg","banner4.jpg", "banner5.jpg"];
+    var rand = Math.round(Math.random() * (imgs.length - 1))
+
+    $(".banner").css({'background-image' : 'url(/images/' + imgs[rand] + ')'});
+
+
     $(".down_arrow").click(function() {
         $("body").animate({ scrollTop: $('#about').offset().top }, 1000);
     });
