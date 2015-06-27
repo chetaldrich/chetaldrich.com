72c9c336732dfe685cab3efd72f7ed5c1a8ae887
chetaldrich <chetaldrich@gmail.com>
2015-06-26 23:45:24 -0700

    Fix a bunch of link errors that showed upon build
    Turns out link_to is generally better practice for HAML.

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     var imgs = ["banner1.jpg","banner2.jpg","banner3.jpg","banner4.jpg", "banner5.jpg"];
     var rand = Math.round(Math.random() * (imgs.length - 1))
 
-    $(".banner").css({'background-image' : 'url(/images/' + imgs[rand] + ')'});
+    $(".banner").css({'background-image' : 'url(images/' + imgs[rand] + ')'});
 
 
     $(".down_arrow").click(function() {
